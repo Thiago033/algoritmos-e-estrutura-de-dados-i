8ca3aab0e1d71240d2ab179e1e705e7339c06a05
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2023-02-12 09:58:09 -0300

    Add and Print names functions

--- exercicios/exercicio-2-agenda/src/main.c
+++ exercicios/exercicio-2-agenda/src/main.c
@@ -1,6 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
-    /* code */
-    return 0;
+// Global
+char *names;
+int letters = 0;
+
+void AddName() {
+    int i = 0;
+    char newName[20];
+
+    system("cls");
+
+    fflush(stdin);
+    printf("Enter a name to add to the list: ");
+    scanf("%s", newName);
+
+   
+    strcat(newName, "/"); // Separate each name by slash(/)
+    
+    
+    names = (char*) realloc( names, sizeof(newName) * sizeof(char) + 2 ); // Reallocate space to a new name
+
+    if ( names == NULL ) {
+        printf("Memory Allocation Error!\n");
+        exit (1);
+    }
+
+    for (letters; letters <= (strlen( names )); letters++) {
+        names[letters] = newName[i];
+        i++;
+    }
+
+    letters--;
+
+    system("cls");
+    printf("Name added to list.\n");
+}
+
+void PrintNames() {
+    printf("%s\n", names);
+}
+
+int main() {
+    int option = 0;
+
+    names = (char*) malloc( sizeof(char) ); 
+
+    if ( names == NULL ) {
+        printf("Memory Allocation Error!\n");
+        exit(1);
+    }
+
+    do {
+        printf("1) Add Name\n");
+        printf("2) Remove Name\n");
+        printf("3) Print Names\n");
+        printf("0) Quit\n");
+        scanf("%d", &option);
+
+        switch ( option ) {
+            case 1:
+                AddName();
+                break;
+            case 2:
+                RemoveName();
+                break;
+            case 3:
+                PrintNames();
+                break;
+            case 0:
+                printf("Quit...");
+                break;
+            default:
+                system("cls");
+                printf("Invalid Option!\n");
+                break;
+        }
+    } while ( option != 0 );
+    
+    free(names);
 }
