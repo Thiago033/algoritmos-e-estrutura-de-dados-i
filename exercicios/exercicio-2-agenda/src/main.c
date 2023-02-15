3fdc25fe40240aa696988cb7e0b17e17d8845132
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2023-02-15 15:03:21 -0300

    Remove name function

--- exercicios/exercicio-2-agenda/src/main.c
+++ exercicios/exercicio-2-agenda/src/main.c
@@ -6,9 +6,16 @@
 char *names;
 int letters = 0;
 
+/*
+=======================================================================================
+AddName
+
+Adds a new name to the string "names"
+=======================================================================================
+*/
 void AddName() {
     int i = 0;
-    char newName[20];
+    char newName[30];
 
     system("cls");
 
@@ -17,17 +24,17 @@ void AddName() {
     scanf("%s", newName);
 
    
-    strcat(newName, "-"); // Separate each name by slash(/)
+    strcat(newName, "/"); // Separate each name by a slash(/)
     
     
-    names = (char*) realloc( names, sizeof(newName) * sizeof(char) + 2 ); // Reallocate space to a new name
+    names = (char*) realloc( names, sizeof(newName) + 2 ); // Reallocate space to a new name
 
     if ( names == NULL ) {
         printf("Memory Allocation Error!\n");
         exit (1);
     }
 
-    for (letters; letters <= (strlen( names )); letters++) {
+    for ( letters; letters <= (strlen( names )); letters++ ) {
         names[letters] = newName[i];
         i++;
     }
@@ -38,21 +45,71 @@ void AddName() {
     printf("Name added to list.\n");
 }
 
+/*
+=======================================================================================
+RemoveName
+
+Removes a specific name from the string "names"
+=======================================================================================
+*/
+void RemoveName(){
+    int index;
+    char nameToDelete[30];
+
+    printf("Enter a name to delete: ");
+    scanf("%s", nameToDelete);
+
+    char *name = strstr(names, nameToDelete); // Finds the pointer to the name to be deleted in the string "names"
+    
+    if ( !name ) {
+        printf("This name doesn't exist on the list!\n");
+    } else {
+        index = strlen( names ) - strlen( name );
+
+        if (index != 0 && names[index - 1] == '/') {
+            index--;
+            name--;
+        }
+
+        for ( int i = 0; i <= strlen( nameToDelete ); i++ ) {
+            for ( int j = 0; j < strlen( name ); j++ ) {
+                names[index + j] = names[index + j + 1];
+            }
+
+            letters--;
+        }
+
+        names = (char *)realloc(names, (strlen( names )) + 2);
+
+        system("cls");
+        printf("Name Deleted\n");
+    }
+}
+
+/*
+=======================================================================================
+PrintNames
+
+Prints on terminal all names on the string "names"
+=======================================================================================
+*/
 void PrintNames() {
     system("cls");
 
+    //printf("%s\n", names);
+
     // Duplicate the string names
-    char *string = strdup(names);
+    char *string = strdup( names );
 
     // get the first token
-    char *name = strtok( string, "-" );
+    char *name = strtok( string, "/" );
 
     // walk through other tokens
     while( name != NULL ) {
         printf("%s\n", name);
 
         // get next token
-        name = strtok( NULL, "-" );
+        name = strtok( NULL, "/" );
     }
 }
 
@@ -78,7 +135,7 @@ int main() {
                 AddName();
                 break;
             case 2:
-                //RemoveName();
+                RemoveName();
                 break;
             case 3:
                 PrintNames();
