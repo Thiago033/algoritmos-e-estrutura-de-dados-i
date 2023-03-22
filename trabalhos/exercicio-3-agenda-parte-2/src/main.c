beedbc3e1a989fbbda51b21b2413dcf58c7a004d
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2023-03-22 20:33:57 -0300

    Add person

--- trabalhos/exercicio-3-agenda-parte-2/src/main.c
+++ trabalhos/exercicio-3-agenda-parte-2/src/main.c
@@ -2,15 +2,152 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NAMESIZE    sizeof(char) * 10 + 1
+#define AGESIZE     sizeof(int)
+#define PHONESIZE   sizeof(int)
+#define NEXT        NAMESIZE + AGESIZE + PHONESIZE
+#define PREVIOUS    NAMESIZE + AGESIZE + PHONESIZE + sizeof(char *)
+#define PERSONSIZE  NAMESIZE + AGESIZE + PHONESIZE + sizeof(char *) + sizeof(char *)
+
 // Main buffer
 void *pBuffer = NULL;
 
+
+// List Pointers
+//  List Start
+char *head = NULL;
+//  list End
+char *last = NULL;
+
 // pBuffer Variables
-int *option = NULL;
+int *option = NULL, *personCounter = NULL, *i = NULL;
+char *tempName  = NULL;
+int *tempAge = NULL, *tempPhone = NULL;
+
+
+/*
+====================================
+NewPerson
+
+    Return a new person
+====================================
+*/
+void* NewPerson() { 
+    void *newPerson = NULL;
+
+    newPerson = (void*)(malloc( PERSONSIZE ));
+
+    if ( newPerson == NULL ) {
+        printf("Error memory allocation");
+        exit (1);
+    }
+
+    printf("Enter the name: \n");
+    fflush(stdin);
+    gets( tempName );
+
+    printf("Enter the age: \n");
+    fflush(stdin);
+    scanf("%d", tempAge);
+
+    printf("Enter the phone number: \n");
+    fflush(stdin);
+    scanf("%d", tempPhone);
+
+    // Person Data
+    strcpy( (char *)(newPerson), tempName );
+    *(int *)(newPerson + NAMESIZE) = *tempAge;
+    *(int *)(newPerson + NAMESIZE + AGESIZE) = *tempPhone;
+
+    // Next Pointer
+    *(char **)(newPerson + NEXT)     = NULL;
+    // Previous Poiter
+    *(char **)(newPerson + PREVIOUS) = NULL;
+
+    return newPerson;
+}
+
+/*
+====================================
+AddPerson
+
+    Add a new person in the list,
+    sorted in alphabetic order
+====================================
+*/
+void AddPerson( char **head ) {
+    char **ptr = head;
+
+    void *newPerson = NewPerson();
+
+    while ( (*ptr != NULL) && strcmp( *ptr, newPerson ) < 1 ) {
+        // Jump to the next person
+        ptr = (char **)(*ptr + NAMESIZE + AGESIZE + PHONESIZE);
+    }
+
+    /*
+    =====================
+    Update Pointers    
+    =====================
+    */
+
+    // Update the "next" pointer of newPerson;
+    *(char **)(newPerson + NAMESIZE + AGESIZE + PHONESIZE) = *ptr;
+
+    // Update the "next" pointer of ptr (element before newPerson);
+    *ptr = newPerson;
+
+    char *ptrPrevious = *head;
+
+    while ( *head != NULL ) {
+        ptrPrevious = *head;
+
+        // Jump to the next person
+        head = (char **)(*head + NAMESIZE + AGESIZE + PHONESIZE);
+
+        if ( *head != NULL ) {
+            // Update "previous pointer of newPerson"
+            *(char **)(*head + NAMESIZE + AGESIZE + PHONESIZE + sizeof(char **)) = ptrPrevious;
+        }
+    }
+
+    last = ptrPrevious;
+    *personCounter = *personCounter + 1;
+}
+
+// void RemovePerson() {
+
+// }
+
+// void PrintList() { 
+
+// }
+
+// void SearchPerson() {
+
+// }
 
 int main( int argc, char const *argv[] ) {
+    // Main Buffer
+    pBuffer = (void *)(malloc( (sizeof(int) * 5) + (sizeof(char) * 10 + 1) ));
 
-    option = (int *) (pBuffer);
+    if ( pBuffer == NULL ) {
+        printf("Error memory allocation");
+        exit (1);
+    }
+
+    // pBuffer Variables Addresses
+    i             = (int *) (pBuffer);
+
+    option        = (int *) (pBuffer + sizeof(int));
+    *option = 0;
+
+    personCounter = (int *) (pBuffer + (sizeof(int) * 2));
+    *personCounter = 0;
+
+    tempName      = (char *)(pBuffer + (sizeof(int) * 3));
+    tempAge       = (int *) (pBuffer + (sizeof(int) * 3) + (sizeof(char) * 10 + 1));
+    tempPhone     = (int *) (pBuffer + (sizeof(int) * 4) + (sizeof(char) * 10 + 1));
 
     do {
         printf("1) Add Person\n");
@@ -22,7 +159,7 @@ int main( int argc, char const *argv[] ) {
 
         switch ( *option ) {
         case 1:
-            //AddPerson(  );
+            AddPerson( &head );
             break;
 
         case 2:
